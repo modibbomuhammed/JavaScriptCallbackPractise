bfcff1a23eaace4d2494525acd42da2d87d8bf84
Muhammed Modibbo <modibbomuhammed@gmail.com>
2023-10-26 11:36:28 +0100

    countDown

--- countdownTimer.js
+++ countdownTimer.js
@@ -2,13 +2,11 @@ module.exports = (time) => {
     const clearTimer = setInterval(() => {
         if (time === 0) {
             console.log("Ring Ring Ring!!!!");
-        } else {
-            console.log(time)
-        }
-        setTimeout(() => {
             clearInterval(clearTimer);
-        }, time * 100);
+        } else {
+            console.log(`timer is counting down for ${time}`)
+        }
         time--;
-    });
+    }, 1000);
 
 }

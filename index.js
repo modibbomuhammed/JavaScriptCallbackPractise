d265808a89f933ee4c8c065e1a182aea78f6746c
Muhammed Modibbo <modibbomuhammed@gmail.com>
2023-10-26 12:49:46 +0100

    promise practise

--- index.js
+++ index.js
@@ -16,5 +16,55 @@ const answer = findIndex([3, 4, 6, 2, 1], (num, index, arr) => num === 1);
 const answer1 = findIndex([5, 11, 13, 8, 6, 7], num => num % 2 === 0);
 
 countDown(5);
+
+// PROMISE EXAMPLE
+const p1 = new Promise((resolve, reject) => {
+    console.log('look at me');
+    resolve([1, 2, 3, 4, 5]);
+});
+
+
+p1.then((result) => console.log({ result }));
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const randomInt = Math.floor(Math.random() * 10)
+        console.log({ randomInt })
+        resolve(randomInt);
+    }, 500)
+})
+
+
+promise.then((data) => {
+    console.log({ data })
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const randomInt22 = Math.floor(Math.random() * 10)
+            console.log({ randomInt22 })
+            resolve(randomInt22);
+        }, 3000);
+    })
+}).then(data2 => console.log({ data2 }))
 // 
 console.log({ finalResult, answer, answer1 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

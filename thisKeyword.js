fb5be77071eefd7cef9aeaf866b2b02a6ed1459d
Muhammed Modibbo <modibbomuhammed@gmail.com>
2023-10-30 18:43:35 +0000

    this keyword done

--- thisKeyword.js
+++ thisKeyword.js
@@ -20,7 +20,36 @@ function invokeMax(cbk, maxAmount) {
     }
 };
 
-module.exports = { sumEvenArguments };
+// Write a function called once which accepts two parameters, a function and a value 
+// for the keyword 'this'.Once should return a new function that can only be invoked once, 
+// with the value of the keyword this in the function set to be the second parameter.
+
+function once(fn, thisArg) {
+    let completed = false;
+    return function () {
+        if (!completed) {
+            completed = true;
+            return fn.apply(thisArg, arguments);
+        }
+    }
+
+};
+
+// Write a function called bind which accepts a function and a value for the keyword this.
+// Bind should return a new function that when invoked, will invoke the function passed to bind 
+// with the correct value of the keyword this.
+// HINT - if you pass more than two parameters to bind, those parameters should be 
+// included as parameters to the inner function when it is invoked.
+// You will have to make use of closure!
+
+const bind = (fn, thisArg, ...arr) => {
+    return function () {
+        const args = [].slice.call(arguments);
+        return fn.bind(thisArg, ...arr, ...args)();
+    }
+};
+
+module.exports = { sumEvenArguments, invokeMax, once, bind };
 
 
 

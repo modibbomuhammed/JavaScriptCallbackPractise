161c0249388fa3c2c78743cc9ffd01442b445208
Muhammed Modibbo <modibbomuhammed@gmail.com>
2023-10-30 17:17:48 +0000

    second challenge

--- thisKeyword.js
+++ thisKeyword.js
@@ -7,5 +7,39 @@ function sumEvenArguments() {
     return args.reduce((acc, next) => (next % 2 === 0) ? acc + next : acc, 0);
 };
 
+// Write a function called invokeMax which accepts a function and a maximum amount.invokeMax 
+// should return a function that when called increments a counter.
+// If the counter is greater than the maximum amount, the inner function should return "Maxed Out"
+
+function invokeMax(cbk, maxAmount) {
+    let counter = 0;
+    return function () {
+        counter++
+        if (counter > maxAmount) return "Maxed Out"
+        return cbk.apply(null, arguments);
+    }
+};
 
 module.exports = { sumEvenArguments };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
